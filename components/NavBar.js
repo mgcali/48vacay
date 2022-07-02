ffd26dc5a263c2bcedcc10b781334fdd48610525
mgcali <mgcali@my.waketech.edu>
2022-07-01 20:01:08 -0400

    Update NavBar.js

--- components/NavBar.js
+++ components/NavBar.js
@@ -6,28 +6,61 @@ import Link from 'next/link';
 
 export default function NavBar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar  bg-white fixed-top navbar-expand-lg navbar-light scrolling-navbar">
+        <div className="container">
+    
+         
         <Link className="navbar-brand" href="/"><Image src={Logo} alt="logo of 48vacay" width={108} height={100}/></Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-          <li className="nav-item active">
-        <Link className="nav-link" href="/about">About Us </Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" href="/services">Services</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" href="/testimonials">Testimonials</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" href="/contact">Contact Us</Link>
-      </li>
+    
+         
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+    
+          
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    
+            
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item p-3">
+                <Link className="nav-link" href="/about">About Us </Link>
+              </li>
+              <li className="nav-item p-3">
+                <Link className="nav-link" href="/services">Services</Link>
+              </li>
+              <li className="nav-item p-3">
+                <Link className="nav-link" href="/testimonials">Testimonials</Link>
+              </li>
+              <li className="nav-item p-3">
+                <Link className="nav-link" href="/contact">Contact Us</Link>
+              </li>
+            </ul>
+    
            
-          </ul>
+            <ul className="navbar-nav nav-flex-icons">
+              <li className="nav-item p-2">
+                <a href="https://www.facebook.com/48Vacay/" class="nav-link" target="_blank">
+                  <i className="fab fa-facebook"></i>
+                </a>
+              </li>
+              <li className="nav-item p-2">
+                <a href="https://www.instagram.com/48Vacay/" class="nav-link" target="_blank">
+                  <i className="fab fa-instagram"></i>
+                </a>
+              </li>
+              <li className="nav-item p-2">
+                <a href="https://www.youtube.com/channel/UCWDPBfmZ7B9hncCz1tjw8Ig" class="nav-link"
+                  target="_blank">
+                  <i className="fab fa-youtube mr-2"></i>
+                </a>
+              </li>
+            </ul>
+    
+          </div>
+    
         </div>
       </nav>
     )
   }
+  
